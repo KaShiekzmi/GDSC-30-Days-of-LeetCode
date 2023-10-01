632519abd8aa12b85159dd5342befe0067425863
Kashif Abbas Kazmi <114513868+KaShiekzmi@users.noreply.github.com>
2023-10-01 12:07:40 +0500

    Time: 0 ms (100%), Space: 44 MB (19.92%) - LeetHub

---
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[2 * n];
+    int i = 0;
+    int j = n;
+    int k = 0;
+
+    while (k < 2 * n) {
+        result[k++] = nums[i++];
+        result[k++] = nums[j++];
+    }
+
+    return result;
+    }
+}
