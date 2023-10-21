6d7e8170e83d6c8ec558b125c807894ff906fbd6
Kashif Abbas Kazmi <114513868+KaShiekzmi@users.noreply.github.com>
2023-10-21 14:39:28 +0500

    Time: 2 ms (84.15%), Space: 46.3 MB (57.89%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n + 1];
+        
+        for (int i = 1; i <= n; i++) {
+            // To count the number of 1's in i, you can use the following formula:
+            // result[i] = result[i / 2] + (i % 2);
+            result[i] = result[i >> 1] + (i & 1);
+        }
+        
+        return result;
+    }
+}
